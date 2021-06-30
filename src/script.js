ec18955af00fd82e3fefe2e42ad6411d16d07d4b
bobbimed <84331517+bobbimed@users.noreply.github.com>
2021-06-30 15:19:40 -0500

    display forecast data using the API

--- src/script.js
+++ src/script.js
@@ -21,28 +21,40 @@ let days = [
 let day = days[now.getDay()];
 h2.innerHTML = `${day} ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
   let forecastElement = document.querySelector("#weekdays");
-  let days = ["Sun", "Mon", "Tues", "Wed"];
+  let forecast = response.data.daily;
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` 
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
         <div class="col-2">
-          <div class="weather-forecast">${day}</div>
+          <div class="weather-forecast">${formatDay(forecastDay.dt)}</div>
           <img
-            src="https://openweathermap.org/img/wn/10d@2x.png"
+            src="https://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
             alt=""
             width="40 "
           />
           <div class="forecast-temperatures">
-            <span class="weekday-high">74°</span>|<span class="weekday-low"
-              >65°</span
+            <span class="weekday-high">${Math.round(
+              forecastDay.temp.max
+            )}°</span>|<span class="weekday-low"
+              >${Math.round(forecastDay.temp.min)}°</span
             >
           </div>
         </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
